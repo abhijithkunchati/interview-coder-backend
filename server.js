715c2bb440d8364fabc1be3c28c664a1dfda41b9
Abhijith Kunchati <65340199+abhijithkunchati@users.noreply.github.com>
2025-05-06 18:36:32 -0700

    updated

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '100mb' }));
 const fs = require('fs');
 const path = require('path');
 
